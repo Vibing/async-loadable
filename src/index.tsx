a62c9bb36df01892e65dd336fdf99f470bed113d
chenLong <Mac@MacdeMacBook-Pro-2.local>
2018-06-15 14:16:54 +0800

    modify

---
+++ src/index.tsx
@@ -0,0 +1,69 @@
+import React, { Component } from 'react';
+
+function AsyncLoader(options: {
+  loader: () => Promise<React.Component>;
+  loading: React.Component;
+}) {
+  if (!options.loading) {
+    throw new Error('AsyncLoader requires a `loading` component');
+  }
+
+  const opts = Object.assign(
+    {
+      loader: null,
+      loading: null
+    },
+    options
+  );
+
+  return class Loadable extends Component<any, any> {
+    constructor(props) {
+      super(props);
+      this.state = {
+        loading: true,
+        component: null,
+        error: null
+      };
+    }
+
+    componentDidMount() {
+      this._loader();
+    }
+
+    render() {
+      const { loading, component, error } = this.state;
+
+      if (loading || error) {
+        return React.createElement(opts.loading, {
+          error,
+          isLoading: loading
+        });
+      } else if (component) {
+        return React.createElement(component, this.props);
+      } else {
+        return null;
+      }
+    }
+
+    /**
+     * 处理import()返回的promise
+     */
+    _loader = async () => {
+      try {
+        const component = await opts.loader();
+        this.setState({
+          loading: false,
+          component: component.default || component
+        });
+      } catch (error) {
+        this.setState({
+          error,
+          loading: false
+        });
+        throw error;
+      }
+    };
+  };
+}
+
+export default AsyncLoader;
---
+++ webpack.config.babel.js
@@ -0,0 +1,45 @@
+import path from 'path';
+import tsImportPluginFactory from 'ts-import-plugin';
+import UglifyJsPlugin from 'uglifyjs-webpack-plugin';
+
+export default {
+  entry: path.resolve(__dirname, './src/index.tsx'),
+  output: {
+    path: path.resolve(__dirname, './lib'),
+    filename: 'index.js'
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js', '.json'],
+    modules: ['node_modules', path.resolve(__dirname, 'web_modules')]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(tsx|ts)$/,
+        loader: 'ts-loader',
+        options: {
+          transpileOnly: true,
+          getCustomTransformers: () => ({
+            before: [
+              tsImportPluginFactory({
+                libraryName: 'antd',
+                libraryDirectory: 'lib'
+              })
+            ]
+          }),
+          compilerOptions: {
+            module: 'es2015'
+          }
+        },
+        exclude: /node_modules/
+      },
+      {
+        test: /\.(js|jsx)?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader?cacheDirectory=true'
+      },
+      { test: /\.json$/, loader: 'json-loader' }
+    ]
+  },
+  plugins: [new UglifyJsPlugin()]
+};
